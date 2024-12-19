9248c3e6cf10450c634c21b9017dc015ed7c92ea
Prabh0079 <prabneha8487@gmail.com>
2024-12-19 15:36:26 -0500

    Initial Commit

---
+++ MyCafe/MyCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCafeApp.swift
+//  MyCafe
+//
+//  Created by User on 19/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCafeTests/MyCafeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCafeTests.swift
+//  MyCafeTests
+//
+//  Created by User on 19/12/2024.
+//
+
+import Testing
+@testable import MyCafe
+
+struct MyCafeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
